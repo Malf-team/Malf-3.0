259138b27af428971cf14afd6069ff181f7b92b4
Joakim Strand <jockejstrand@gmail.com>
2022-12-13 18:51:41 +0100

    Added custom ores and textures
    Deepslate, nether and end variants missing for now

---
+++ kubejs/startup_scripts/malf_custom_ores.js
@@ -0,0 +1,92 @@
+// priority: 99
+// This file adds ore blocks, raw ore, dusts and ingots (maybe more depending on the ore)
+
+console.info('Hello, World! (You will only see this line once in console, during startup)')
+
+const ore_names = ["iron", "copper", "gold", "aluminum", "lead", "silver", "nickel", "uranium", "tin", "zinc"]
+
+const malf_common_ores = ["hematite", "tetrahedrite", "gold", "cinnabar", "lazurite", "kimberlite", "beryl", "cassiterite", "bauxite", "pentlandite", "galena", "pitchblende", "sphalerite", "argentite"]
+const malf_exotic_ores = ["ethereal_bronze"]
+
+const malf_ore_hardness = {"ethereal_bronze":4}
+const malf_ore_mining_level = {"ethereal_bronze":"diamond"}
+const malf_ore_luminosity = {"ethereal_bronze":0.2, "pitchblende":0.05} //0.0 ~ 1.0
+
+function capitalizeFirstLetter(string) { //Capitalizes first letter
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+function fancify(name) { //Capitalizes and adds spaces to an item id name ('lapis_lazuli' = 'Lapis Lazuli', 'redstone' = 'Redstone', etc)
+	var word = name.split("_")
+	var final_string = ""
+
+	for (var i = 0; i < word.length; i++) {
+		if (word[i] === "deepslate") {word[i] = "slate"} //Shortens deepslate to slate
+		word[i] = capitalizeFirstLetter(word[i]);
+
+		if (i !== 0) { final_string += (" " + word[i]) } //Add space before the word, unless first word
+		else { final_string += (word[i]) }
+	}
+	return final_string
+}
+
+
+onEvent('item.registry', event => {
+	
+	// Register new items here
+	// event.create('example_item').displayName('Example Item')
+
+	function setupWashedOre(item) {
+		event.create('malf:washed_'+item+"_ore").displayName("Washed "+capitalizeFirstLetter(item)+" Ore")
+	}
+	ore_names.forEach(setupWashedOre)
+	
+	//Rare earth
+	event.create('malf:rare_earth_dust').displayName("Rare Earth Dust")
+	event.create('malf:rare_earth_mineral').displayName("Rare Earth Mineral")
+	
+	
+})
+
+onEvent('block.registry', event => {
+	// Register new blocks here
+	// event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
+
+	function createOreBlocks(name) { // Creates a new ore block with all listed variants
+		var variants = ["", "_deepslate", "_nether", "_end"]; // ("" = normal variant), needs a "_" before the name (for technical reasons!)
+
+		var hardness = 3 // Standard hardness
+		if (typeof malf_ore_hardness[name] !== 'undefined') { hardness = malf_ore_hardness[name] } // Sets hardness if defined
+
+    	var mining_level = 'iron' // Standard mining level
+		if (typeof malf_ore_mining_level[name] !== 'undefined') { mining_level = malf_ore_mining_level[name] } // Sets mining level if defined
+
+		var luminosity = 0 // Standard luminosity
+		if (typeof malf_ore_luminosity[name] !== 'undefined') { luminosity = malf_ore_luminosity[name] } // Sets hardness if defined
+		
+		for (var i = 0; i < variants.length; i++) {
+			var full_name = name + variants[i] + "_ore"
+
+			if (variants[i] === "_deepslate") { hardness += 1 }
+
+			event.create('malf:' + full_name)
+				.material('stone')
+				.hardness(hardness)
+				.displayName(fancify(full_name))
+				.tagBlock('minecraft:mineable/pickaxe') // Make it mine faster using a shovel in 1.18.2+
+				.tagBlock('minecraft:needs_' + mining_level + '_tool') // Make it require an iron or higher level tool on 1.18.2+
+				.requiresTool(true)
+				.tagBoth('forge:ores')
+				.tagBoth('forge:ores/' + name)
+				.lightLevel(luminosity)
+		}
+	}
+
+	malf_common_ores.forEach(createOreBlocks);
+	malf_exotic_ores.forEach(createOreBlocks);
+	
+})
+
+onEvent('item.modification', event => {
+
+
+})
--- kubejs/startup_scripts/script.js
+++ kubejs/startup_scripts/script.js
@@ -3,7 +3,6 @@
 console.info('Hello, World! (You will only see this line once in console, during startup)')
 
 onEvent('item.registry', event => {
-	const oreNames = ["iron", "copper", "gold", "aluminum", "lead", "silver", "nickel", "uranium", "tin", "zinc"]
 	// Register new items here
 	// event.create('example_item').displayName('Example Item')
 	event.create('malf:palt_of_the_cosmos').maxStackSize(8).glow(true).displayName("Palt of the Cosmos").food(food => {
@@ -19,64 +18,11 @@ onEvent('item.registry', event => {
 	})
 	
 	
-	function capitalizeFirstLetter(string) {
-		return string.charAt(0).toUpperCase() + string.slice(1);
-	}
-	
-	function setupWashedOre(item) {
-		event.create('malf:washed_'+item+"_ore").displayName("Washed "+capitalizeFirstLetter(item)+" Ore")
-	}
-	oreNames.forEach(setupWashedOre)
-	
-	//Rare earth
-	event.create('malf:rare_earth_dust').displayName("Rare Earth Dust")
-	event.create('malf:rare_earth_mineral').displayName("Rare Earth Mineral")
-	
-	
 })
 
 onEvent('block.registry', event => {
 	// Register new blocks here
 	// event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
-	event.create('malf:rare_earth_ore')
-		   .material('stone')
-		   .hardness(3)
-		   .displayName('Rare Earth Ore') // No longer required in 1.18.2+
-		   .tagBlock('minecraft:mineable/pickaxe') // Make it mine faster using a shovel in 1.18.2+
-		   .tagBlock('minecraft:requires_iron_tool') // Make it require an iron or higher level tool on 1.18.2+
-		   .requiresTool(true) // Make it require a tool to drop ay loot
-		   .tagBoth('forge:ores')
-		   .tagBoth('forge:ores/rare_earth')
-		   
-	event.create('malf:deepslate_rare_earth_ore')
-		   .material('stone')
-		   .hardness(4.5)
-		   .displayName('Rare Earth Slate Ore') // No longer required in 1.18.2+
-		   .tagBlock('minecraft:mineable/pickaxe') // Make it mine faster using a shovel in 1.18.2+
-		   .tagBlock('minecraft:requires_iron_tool') // Make it require an iron or higher level tool on 1.18.2+
-		   .requiresTool(true) // Make it require a tool to drop ay loot
-		   .tagBoth('forge:ores')
-		   .tagBoth('forge:ores/rare_earth')
-		   
-	event.create('malf:nether_rare_earth_ore')
-		   .material('stone')
-		   .hardness(3)
-		   .displayName('Rare Earth Nether Ore') // No longer required in 1.18.2+
-		   .tagBlock('minecraft:mineable/pickaxe') // Make it mine faster using a shovel in 1.18.2+
-		   .tagBlock('minecraft:requires_iron_tool') // Make it require an iron or higher level tool on 1.18.2+
-		   .requiresTool(true) // Make it require a tool to drop ay loot
-		   .tagBoth('forge:ores')
-		   .tagBoth('forge:ores/rare_earth')
-		   
-	event.create('malf:end_rare_earth_ore')
-		   .material('stone')
-		   .hardness(3)
-		   .displayName('Rare Earth End Ore') // No longer required in 1.18.2+
-		   .tagBlock('minecraft:mineable/pickaxe') // Make it mine faster using a shovel in 1.18.2+
-		   .tagBlock('minecraft:requires_iron_tool') // Make it require an iron or higher level tool on 1.18.2+
-		   .requiresTool(true) // Make it require a tool to drop ay loot
-		   .tagBoth('forge:ores')
-		   .tagBoth('forge:ores/rare_earth')
 })
 
 onEvent('item.modification', event => {
