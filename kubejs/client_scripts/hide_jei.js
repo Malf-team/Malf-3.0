e32d0c80d93659eab8f37bb666f1497b986d36fa
Joakim Strand <jockejstrand@gmail.com>
2022-12-19 05:21:19 +0100

    Hide alltheores ores

--- kubejs/client_scripts/hide_jei.js
+++ kubejs/client_scripts/hide_jei.js
@@ -12,7 +12,34 @@ onEvent('jei.hide.items', (event) => {
 		"alltheores:bronze_ore_hammer",
 		"alltheores:invar_ore_hammer",
 		"alltheores:platinum_ore_hammer",
+		"alltheores:ruby_ore",
+		"alltheores:ruby_slate_ore",
+		"alltheores:peridot_ore",
+		"alltheores:peridot_slate_ore",
+		"alltheores:sapphire_ore",
+		"alltheores:sapphire_slate_ore",
 	]
+
+	const ores_to_remove = ["aluminum", "lead", "nickel", "osmium", "platinum", "silver", "tin", "uranium", "zinc", "iridium"]
+	const other_ores_to_remove = ["coal", "copper", "emerald", "iron", "quartz", "redstone", "diamond", "lapis", "platnium"]
+
+	function hide_alltheores_ore(name) {
+		hide.push("alltheores:"+name+"_ore")
+		hide.push("alltheores:"+name+"_slate_ore")
+		hide.push("alltheores:"+name+"_nether_ore")
+		hide.push("alltheores:"+name+"_end_ore")
+		hide.push("alltheores:raw+"+name)
+		hide.push("alltheores:raw+"+name+"_block")
+	}
+
+	function hide_other_ore(name) {
+		hide.push("alltheores:"+name+"_other_ore")
+	}
+
+	ores_to_remove.forEach(hide_alltheores_ore)
+	other_ores_to_remove.forEach(hide_other_ore)
+
+
 
 	hide.forEach((id) => {
 		event.hide(id)
