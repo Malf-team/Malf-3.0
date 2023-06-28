8ec3da481c951cb339ee8c18f9c465f1cfff7c63
WolframRele <42933066+WolframRele@users.noreply.github.com>
2023-06-28 15:39:48 +0200

    Two quest Added
    added two quests for the building questline

--- kubejs/server_scripts/skills.js
+++ kubejs/server_scripts/skills.js
@@ -1,11 +1,124 @@
 ItemSkillEvents.register((event) => {
-    event.restrict('minecraft:*', restrict => {
-        restrict.everything().if(player => player.cannot('skills:malftest', true));
+    event.restrict('minecraft:oak_door', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build1'))
     })
-});
-
-/*BlockEvents.rightClicked((event) => {
-    event.improve('skills:malftest', (conditions) => {
-        conditions.chance(50);
+    event.restrict('minecraft:spruce_door', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build1'))
     })
-});*/
+    event.restrict('minecraft:birch_door', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build1'))
+    })
+    event.restrict('minecraft:jungle_door', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build1'))
+    })
+    event.restrict('minecraft:acacia_door', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build1'))
+    })
+    event.restrict('minecraft:dark_oak_door', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build1'))
+    })
+    event.restrict('minecraft:mangrove_door', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build1'))
+    })
+    event.restrict('minecraft:bowl', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build1'))
+    })
+    event.restrict('minecraft:chest', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build1'))
+    })
+    event.restrict('minecraft:packed_mud', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build1'))
+    })
+    event.restrict('minecraft:oak_slab', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build1'))
+    })
+    event.restrict('minecraft:spruce_slab', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build1'))
+    })
+    event.restrict('minecraft:birch_slab', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build1'))
+    })
+    event.restrict('minecraft:jungle_slab', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build1'))
+    })
+    event.restrict('minecraft:acacia_slab', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build1'))
+    })
+    event.restrict('minecraft:dark_oak_slab', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build1'))
+    })
+    event.restrict('minecraft:mangrove_slab', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build1'))
+    })
+})
+ItemSkillEvents.register((event) => {
+    event.restrict('minecraft:oak_trapdoor', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+    event.restrict('minecraft:spruce_trapdoor', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+    event.restrict('minecraft:birch_trapdoor', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+    event.restrict('minecraft:jungle_trapdoor', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+    event.restrict('minecraft:acacia_trapdoor', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+    event.restrict('minecraft:dark_oak_trapdoor', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+    event.restrict('minecraft:mangrove_trapdoor', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+    event.restrict('minecraft:armor_stand', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+    event.restrict('minecraft:scaffolding', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+    event.restrict('minecraft:oak_fence', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+    event.restrict('minecraft:spruce_fence', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+    event.restrict('minecraft:birch_fence', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+    event.restrict('minecraft:jungle_fence', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+    event.restrict('minecraft:acacia_fence', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+    event.restrict('minecraft:dark_oak_fence', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+    event.restrict('minecraft:mangrove_fence', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+    event.restrict('minecraft:oak_fence_gate', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+    event.restrict('minecraft:spruce_fence_gate', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+    event.restrict('minecraft:birch_fence_gate', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+    event.restrict('minecraft:jungle_fence_gate', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+    event.restrict('minecraft:acacia_fence_gate', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+    event.restrict('minecraft:dark_oak_fence_gate', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+    event.restrict('minecraft:mangrove_fence_gate', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build2_1'))
+    })
+})
--- kubejs/startup_scripts/skills.js
+++ kubejs/startup_scripts/skills.js
@@ -1,6 +1,18 @@
+/*
 SkillEvents.registration((event) => {
     event.add('skills:malftest', 'skills:basic', (skill) => {
         skill.initialValue(false);
         skill.description("Player has joined malf");
     });
 });
+*/
+SkillEvents.registration((event) => {
+    event.add('skills:build1', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player Has Made Planks")
+    });
+    event.add('skills:build2_1', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player Has Made A door")
+    });
+}); 
