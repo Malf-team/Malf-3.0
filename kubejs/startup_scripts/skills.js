081ed679cbd5a89e9bd3229ac5262c8000cb6a7e
WolframRele <42933066+WolframRele@users.noreply.github.com>
2023-06-30 02:37:33 +0200

    Uppdated Build tree
    added build skills 3_1 and 4_1

--- kubejs/startup_scripts/skills.js
+++ kubejs/startup_scripts/skills.js
@@ -9,10 +9,94 @@ SkillEvents.registration((event) => {
 SkillEvents.registration((event) => {
     event.add('skills:build1', 'skills:basic', (skill) => {
         skill.initialValue(false);
-        skill.description("Player Has Made Planks")
+        skill.description("Player has made Planks")
     });
     event.add('skills:build2_1', 'skills:basic', (skill) => {
         skill.initialValue(false);
-        skill.description("Player Has Made A door")
+        skill.description("Player has made a Door")
+    });
+    event.add('skills:build3_1', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player has made a Fences")
+    });
+    event.add('skills:build4_1', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player has made a Bookshelf")
+    });
+    event.add('skills:build5_1', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player has made Mushroom planks")
+    });
+    event.add('skills:build2_2', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player has made Packed Mud")
+    });
+    event.add('skills:build3_2', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player has made a Brick")
+    });
+    event.add('skills:build4_2_2', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player has made a Stonecutter")
+    });
+    event.add('skills:build5_2_2_2', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player has made a Cut Copper")
+    });
+    event.add('skills:build5_2_2_3', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player has made a Concrete")
+    });
+    event.add('skills:build6_2_2_3', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player has made a Terracota")
+    });
+    event.add('skills:build5_2_2_4', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player has made a Cut Sand Stone")
+    });
+    event.add('skills:build5_2_2_1', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player has made a Stone Stairs")
+    });
+    event.add('skills:build6_2_2_1', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player has made a Ciseld")
+    });
+    event.add('skills:build4_2_1', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player has made a Bricks")
+    });
+    event.add('skills:build5_2_1', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player has made a Nether Brick")
+    });
+    event.add('skills:build6_2_1', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player has made a Deepslate Bricks")
+    });
+    event.add('skills:build2_3', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player has made a Bowl")
+    });
+    event.add('skills:build3_3_2', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player has made a Prismarine")
+    });
+    event.add('skills:build4_3_2', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player has made a Prismarine Brick")
+    });
+    event.add('skills:build5_3_2', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player has made a Dark Prismarine")
+    });
+    event.add('skills:build6_3_2', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player has made a Quartz Bricks")
+    });
+    event.add('skills:build3_3_1', 'skills:basic', (skill) => {
+        skill.initialValue(false);
+        skill.description("Player has made a Item Fram")
     });
 }); 
