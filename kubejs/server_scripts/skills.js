4f57de42005f82c5c267aca5a37c6adc8c92f179
WolframRele <42933066+WolframRele@users.noreply.github.com>
2023-06-30 02:37:41 +0200

    Update skills.js

--- kubejs/server_scripts/skills.js
+++ kubejs/server_scripts/skills.js
@@ -122,3 +122,112 @@ ItemSkillEvents.register((event) => {
         restrict.unproducible().if(player => player.cannot('skills:build2_1'))
     })
 })
+ItemSkillEvents.register((event) => {
+    event.restrict('minecraft:oak_sign', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build3_1'))
+    })
+    event.restrict('minecraft:spruce_sign', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build3_1'))
+    })
+    event.restrict('minecraft:birch_sign', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build3_1'))
+    })
+    event.restrict('minecraft:jungle_sign', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build3_1'))
+    })
+    event.restrict('minecraft:acacia_sign', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build3_1'))
+    })
+    event.restrict('minecraft:dark_oak_sign', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build3_1'))
+    })
+    event.restrict('minecraft:mangrove_sign', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build3_1'))
+    })
+    event.restrict('minecraft:barrel', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build3_1'))
+    })
+    event.restrict('minecraft:bookshelf', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build3_1'))
+    })
+    event.restrict('minecraft:lectern', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build3_1'))
+    })
+    event.restrict('minecraft:oak_stairs', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build3_1'))
+    })
+    event.restrict('minecraft:spruce_stairs', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build3_1'))
+    })
+    event.restrict('minecraft:birch_stairs', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build3_1'))
+    })
+    event.restrict('minecraft:jungle_stairs', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build3_1'))
+    })
+    event.restrict('minecraft:acacia_stairs', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build3_1'))
+    })
+    event.restrict('minecraft:dark_oak_stairs', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build3_1'))
+    })
+    event.restrict('minecraft:mangrove_stairs', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build3_1'))
+    })
+})
+ItemSkillEvents.register((event) => {
+    event.restrict('minecraft:crimson_sign', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build4_1'))
+    })
+    event.restrict('minecraft:warped_sign', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build4_1'))
+    })
+    event.restrict('minecraft:crimson_planks', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build4_1'))
+    })
+    event.restrict('minecraft:warped_planks', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build4_1'))
+    })
+    event.restrict('minecraft:crimson_door', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build4_1'))
+    })
+    event.restrict('minecraft:warped_door', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build4_1'))
+    })
+    event.restrict('minecraft:crimson_fence', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build4_1'))
+    })
+    event.restrict('minecraft:warped_fence', restrict => {
+        restrict.unproducible().if(player => player.cannot('skills:build4_1'))
+    })
+})
+BlockSkillEvents.register(event => {
+    event.restrict('minecraft:oak_log', (is) => {
+        is.unusable().if(player => player.cannot('skills:build4_1'))
+    })
+    event.restrict('minecraft:spruce_log', (is) => {
+        is.unusable().if(player => player.cannot('skills:build4_1'))
+    })
+    event.restrict('minecraft:birch_log', (is) => {
+        is.unusable().if(player => player.cannot('skills:build4_1'))
+    })
+    event.restrict('minecraft:jungle_log', (is) => {
+        is.unusable().if(player => player.cannot('skills:build4_1'))
+    })
+    event.restrict('minecraft:acacia_log', (is) => {
+        is.unusable().if(player => player.cannot('skills:build4_1'))
+    })
+    event.restrict('minecraft:dark_oak_log', (is) => {
+        is.unusable().if(player => player.cannot('skills:build4_1'))
+    })
+    event.restrict('minecraft:mangrove_log', (is) => {
+        is.unusable().if(player => player.cannot('skills:build4_1'))
+    })
+    event.restrict('minecraft:crimson_stem', (is) => {
+        is.unusable().if(player => player.cannot('skills:build4_1'))
+    })
+    event.restrict('minecraft:warped_stem', (is) => {
+        is.unusable().if(player => player.cannot('skills:build4_1'))
+    })
+
+})
